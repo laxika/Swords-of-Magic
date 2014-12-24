e4ca5fd244add7ad147a637e2af869644879d9f7
laxika <laxika91@gmail.com>
2014-12-24 15:13:48 +0100

    base for language change

--- src/main/java/com/swords/Application.java
+++ src/main/java/com/swords/Application.java
@@ -1,18 +1,26 @@
 package com.swords;
 
 import com.swords.filter.SessionLoginFilter;
-import java.util.ArrayList;
+import com.swords.interceptor.SessionLocaleChangeInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 @EnableAutoConfiguration
 @Configuration
 @ComponentScan
-public class Application {
+public class Application extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
@@ -31,5 +39,23 @@ public class Application {
         registrationBean.setUrlPatterns(urlPatterns);
 
         return registrationBean;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        return new SessionLocaleChangeInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
---
+++ src/main/java/com/swords/interceptor/SessionLocaleChangeInterceptor.java
@@ -0,0 +1,29 @@
+package com.swords.interceptor;
+
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.support.RequestContextUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+public class SessionLocaleChangeInterceptor extends LocaleChangeInterceptor {
+
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException {
+        Locale newLocale = (Locale) request.getSession().getAttribute("lang");
+
+        if (newLocale != null) {
+            LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver(request);
+            if (localeResolver == null) {
+                throw new IllegalStateException("No LocaleResolver found: not in a DispatcherServlet request?");
+            }
+
+            localeResolver.setLocale(request, response, newLocale);
+        }
+
+        return true;
+    }
+
+}

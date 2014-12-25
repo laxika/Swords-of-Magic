08958eec2bfa80eca7c2fe27bd95a3cc08a8865f
laxika <laxika91@gmail.com>
2014-12-25 14:02:12 +0100

    fixed the rarity detection

--- src/main/java/com/swords/model/constant/RarityConstant.java
+++ src/main/java/com/swords/model/constant/RarityConstant.java
@@ -2,7 +2,7 @@ package com.swords.model.constant;
 
 public class RarityConstant {
 
-    public static final String MYTHIC = "Mythic";
+    public static final String MYTHIC = "Mythic Rare";
     public static final String RARE = "Rare";
     public static final String UNCOMMON = "Uncommon";
     public static final String COMMON = "Common";
